cec513f8a75d3de3990fa413b944461b02e97dd5
Noah Luck Easterly <noah.easterly@gmail.com>
2017-04-14 22:38:44 -0400

    Implement Positive

--- src/Data/Ordinal/Positive.hs
+++ src/Data/Ordinal/Positive.hs
@@ -1,6 +1,27 @@
 module Data.Ordinal.Positive where
 
-newtype Positive a = Positive { fromPositive :: a }
+import Prelude hiding (map)
+import Data.Maybe (fromMaybe)
 
-toPositive :: a -> Maybe (Positive a)
-toPositive = undefined
+newtype Positive a = Positive { fromPositive :: a }
+  deriving (Eq, Ord)
+
+toPositive :: (Num a, Ord a) => a -> Maybe (Positive a)
+toPositive a | a <= 0    = Nothing
+             | otherwise = Just $ Positive a
+
+instance (Num a, Ord a) => Num (Positive a) where
+  (+) = apply (+)
+  (*) = apply (*)
+  (-) = error "subtraction is not defined for Positive numbers"
+  negate = error "negation is not defined for Positive numbers"
+  abs = id
+  signum = map signum
+  fromInteger n = fromMaybe (error msg) . toPositive $ fromInteger n  where
+    msg = shows n " can not be converted to a Positive number"
+
+apply :: (a -> a -> a) -> Positive a -> Positive a -> Positive a
+apply f (Positive a) (Positive b) = Positive $ f a b
+
+map :: (a -> a) -> Positive a -> Positive a
+map f (Positive a) = Positive $ f a

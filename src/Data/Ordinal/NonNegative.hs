2178d5f001831258019d62ffba8d4d4065bbf8e1
Noah Luck Easterly <noah.easterly@gmail.com>
2017-04-14 22:38:54 -0400

    Implement NonNegative

--- src/Data/Ordinal/NonNegative.hs
+++ src/Data/Ordinal/NonNegative.hs
@@ -1,6 +1,27 @@
 module Data.Ordinal.NonNegative where
 
-newtype NonNegative a = NonNegative { fromNonNegative :: a }
+import Prelude hiding (map)
+import Data.Maybe (fromMaybe)
 
-toNonNegative :: a -> Maybe (NonNegative a)
-toNonNegative = undefined
+newtype NonNegative a = NonNegative { fromNonNegative :: a }
+  deriving (Eq, Ord)
+
+toNonNegative :: (Num a, Ord a) => a -> Maybe (NonNegative a)
+toNonNegative a | a < 0     = Nothing
+                | otherwise = Just $ NonNegative a
+
+instance (Num a, Ord a) => Num (NonNegative a) where
+  (+) = apply (+)
+  (*) = apply (*)
+  (-) = error "subtraction is not defined for NonNegative numbers"
+  negate = error "negation is not defined for NonNegative numbers"
+  abs = id
+  signum = map signum
+  fromInteger n = fromMaybe (error msg) . toNonNegative $ fromInteger n  where
+    msg = shows n " can not be converted to a NonNegative number"
+
+apply :: (a -> a -> a) -> NonNegative a -> NonNegative a -> NonNegative a
+apply f (NonNegative a) (NonNegative b) = NonNegative $ f a b
+
+map :: (a -> a) -> NonNegative a -> NonNegative a
+map f (NonNegative a) = NonNegative $ f a

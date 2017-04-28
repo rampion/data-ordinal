33c175e77d68fdf46591a25c316d8610ee21cd1c
Noah Luck Easterly <noah.easterly@gmail.com>
2017-04-28 17:40:40 -0400

    Implement Ord for Kleene

--- src/Data/Ordinal/Kleene/Internal.hs
+++ src/Data/Ordinal/Kleene/Internal.hs
@@ -7,41 +7,82 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE ConstraintKinds #-}
 module Data.Ordinal.Kleene.Internal where 
 
-import GHC.Exts (Constraint)
+-- import GHC.Exts (Constraint)
 import Data.Ordinal.Zero
 import Data.Ordinal.Lens
+
+type Derived a = (HasZero a, Ord a, Num a)
 
 -- think of Kleene and IsKleene it as lists of dictionaries in opposite order
 
 data Kleene t b where
-  Pure :: HasZero b => b -> Kleene t b
-  Wrap :: HasZero b => Kleene t (t b) -> Kleene t b
+  Pure :: Derived b => b -> Kleene t b
+  Wrap :: Derived b => Kleene t (t b) -> Kleene t b
 
-data IsKleene a t b where
-  Refl :: HasZero a => IsKleene a t a
-  Impl :: HasZero (t a) => IsKleene a t b -> IsKleene (t a) t b
+peekKleene :: Kleene t b -> (Derived b => x) -> x
+peekKleene (Pure _) x = x
+peekKleene (Wrap _) x = x
 
-data ViewKleene t b where
-  ViewKleene :: IsKleene a t b -> a -> ViewKleene t b
-
-instance HasZero b => HasZero (Kleene t b) where
+instance Derived b => HasZero (Kleene t b) where
   isZero (Pure Zero) = True
   isZero _ = False
   zero = Pure Zero
 
-instance HasZero b => HasZero (ViewKleene t b) where
+instance LensBase t => Eq (Kleene t b) where
+  j == k = case j `compare` k of EQ -> True ; _ -> False
+
+instance LensBase t => Ord (Kleene t b) where
+  compare = bicata compare
+
+bicata :: LensBase t => (forall a. Derived a => a -> a -> x) -> Kleene t b -> Kleene t b -> x
+bicata f (Wrap j) (Wrap k) = bicata f j k
+bicata f (Pure a) (Wrap k) = k `peekKleene` bicata f (Pure $ fromBase a) k
+bicata f (Wrap j) (Pure b) = j `peekKleene` bicata f j (Pure $ fromBase b)
+bicata f (Pure a) (Pure b) = f a b
+
+instance LensBase t => Num (Kleene t b) where
+  -- j + k = fromViewKleene $ bicata (\a b -> toViewKleene $ a + b) j k
+  Wrap j + Wrap k = Wrap $ j + k
+  Pure a + Wrap k = peekKleene k $ Wrap $ Pure (fromBase a) + k
+  Wrap j + Pure b = peekKleene j $ Wrap $ j + Pure (fromBase b)
+  Pure a + Pure b = Pure $ a + b
+  (*) = undefined
+  (-) = undefined
+  negate = undefined
+  abs = undefined
+  signum = undefined
+  fromInteger = undefined
+  
+data IsKleene a t b where
+  Refl :: Derived a => IsKleene a t a
+  Impl :: Derived (t a) => IsKleene a t b -> IsKleene (t a) t b
+
+data ViewKleene t b where
+  ViewKleene :: IsKleene a t b -> a -> ViewKleene t b
+
+instance Derived b => HasZero (ViewKleene t b) where
   isZero (ViewKleene Refl Zero) = True
   isZero _ = False
   zero = ViewKleene Refl Zero
 
+{-
+instance Eq (ViewKleene t b) where
+  vj == vk = case vj `compare` vk of EQ -> True ; _ -> False
+
+instance Ord (ViewKleene t b) where
+-}
+  
 -- | smart constructor
 toViewKleene :: forall a t b. (ToKleene a t b, LensBase t) => a -> ViewKleene t b
-toViewKleene = simplify isKleene where
-  simplify :: forall x. IsKleene x t b -> x -> ViewKleene t b
-  simplify (Impl pf@(starHasZero -> QED)) (viewBase -> (x, Zero)) = simplify pf x
-  simplify pf x = ViewKleene pf x
+toViewKleene = simplify isKleene
+
+simplify :: LensBase t => IsKleene a t b -> a -> ViewKleene t b
+simplify (Impl pf@Refl) (viewBase -> (a, Zero)) = simplify pf a
+simplify (Impl pf@(Impl _)) (viewBase -> (a, Zero)) = simplify pf a
+simplify pf a = ViewKleene pf a
 
 -- | smart constructor
 toKleene :: forall a t b. (ToKleene a t b, LensBase t) => a -> Kleene t b
@@ -50,198 +91,26 @@ toKleene = fromViewKleene . toViewKleene
 -- fromViewKleene & fromKleene traverse the list of dictionaries and reverse it
 
 fromViewKleene :: forall t b. ViewKleene t b -> Kleene t b
-fromViewKleene = \(ViewKleene pf a) -> starHasZero pf `implies` wrap pf (Pure a) where
+fromViewKleene = check where
+  check (ViewKleene pf@Refl a) = wrap pf (Pure a)
+  check (ViewKleene pf@(Impl _) a) = wrap pf (Pure a)
+
   wrap :: forall x y. IsKleene x t y -> Kleene t x -> Kleene t y
-  wrap (Impl pf) k = starHasZero pf `implies` wrap pf (Wrap k)
+  wrap (Impl pf@Refl) k = wrap pf (Wrap k)
+  wrap (Impl pf@(Impl _)) k = wrap pf (Wrap k)
   wrap Refl k = k
 
 fromKleene :: forall t b. Kleene t b -> ViewKleene t b
-fromKleene = \k -> baseHasZero k `implies` unwrap k Refl where
+fromKleene = \k -> k `peekKleene` unwrap k Refl where
   unwrap :: forall x y. Kleene t x -> IsKleene x t y -> ViewKleene t y
-  unwrap (Wrap k) pf = baseHasZero k `implies` unwrap k (Impl pf)
+  unwrap (Wrap k) pf = k `peekKleene` unwrap k (Impl pf)
   unwrap (Pure b) pf = ViewKleene pf b
 
-starHasZero :: IsKleene a t b -> Claim '[HasZero] a
-starHasZero (Impl _) = QED
-starHasZero Refl = QED
-
-baseHasZero :: Kleene t b -> Claim '[HasZero] b
-baseHasZero (Wrap _) = QED
-baseHasZero (Pure _) = QED
-
-class ToKleene a t b where
+class Derived a => ToKleene a t b where
   isKleene :: IsKleene a t b
 
-instance HasZero a => ToKleene a t a where
+instance Derived a => ToKleene a t a where
   isKleene = Refl
 
-instance (HasZero (t a), ToKleene a t b) => ToKleene (t a) t b where
+instance (Derived (t a), ToKleene a t b) => ToKleene (t a) t b where
   isKleene = Impl isKleene
-
-data Claim (cs :: [* -> Constraint]) (a :: *) where
-  QED :: All cs a => Claim cs a
-
-type family All (cs :: [* -> Constraint]) (a :: *) :: Constraint where
-  All '[] a = ()
-  All (c ': cs) a = (c a, All cs a)
-
-implies :: Claim cs a -> (All cs a => x) -> x
-QED `implies` x = x
-{-
-class Preserves (cs :: [* -> Constraint]) (t :: * -> *) where
-  preserves :: All cs a => Claim cs (t a)
-
--}
-
-
-{-
-toKleene' :: forall a b t. 
-  (ToKleene a t b, HasZero b, HasZero a, LensBase t) => 
-  a -> Kleene t b
-toKleene' = test where
-  test :: a -> Kleene t b
-  test Zero = Zero
-  test a = loop (toKleene a) a
-
-  loop :: forall x. Kleene t b -> x -> Kleene t b
-  loop (Wrap _) (viewBase -> (a, Zero)) = loop (toKleene a) a
-  loop k _ = k
--}
-
-
-
-{-
-data Kleene cs t b where
-  Kleene :: a `IsSome` t `Of` b `With` cs -> a -> Kleene cs t b
-
-instance HasZero b => HasZero (Kleene '[HasZero] t b) where
-  zero = Kleene Reflexive zero
-  isZero (Kleene Reflexive b) = isZero b
-  isZero _ = False
-
-class Derives cs t where
-  derives :: Satisfied cs a -> Satisfied cs (t a)
-
-
--- | smart constructor
-toKleene :: forall a b t. (HasZero b, LensBase t, Derives '[HasZero] t) => a `IsSome` t `Of` b -> a -> Kleene t b
-toKleene = \pf -> hasZero pf `implies` test pf where
-  hasZero :: forall x. x `IsSome` t `Of` b -> Satisfied '[HasZero] x
-  hasZero = induction derives
-
-  test :: HasZero a => a `IsSome` t `Of` b -> a -> Kleene t b
-  test _ Zero = Zero
-  test pf a = loop pf a
-
-  -- a is nonzero
-  loop :: forall x. HasZero x => x `IsSome` t `Of` b -> x -> Kleene t b
-  loop Reflexive a = Kleene Reflexive a
-  loop (Inductive pf) ta = hasZero pf `implies` case viewBase ta of
-    (a, Zero) -> loop pf a
-    _         -> Kleene (Inductive pf) ta
-
--}
-
-{-
---import GHC.Exts (Constraint)
-import Data.Ordinal.Finite
-import Data.Functor.Identity (Identity(..))
-import Data.Functor.Product (Product(..))
--- import Data.Ordinal.Positive.Internal
--- import Data.Ordinal.Expansion
--- import Data.Ordinal.Minus
--- import Data.Ordinal.LPred
-
-
-type x `IsSomeExpansionOf` a = x `IsSome` Expansion `Of` a
-
-data AnyExpansionF f a where
-  AnyExpansionF :: x `IsSomeExpansionOf` a -> f x -> AnyExpansionF f a
-
-type AnyExpansion = AnyExpansionF Identity
-
-{-
--- | smart constructor
-toAnyExpansion :: (Num a, a `HasExpansion` b) => b -> AnyExpansion a
-toAnyExpansion = lower hasExpansion
--}
-
-lower :: Num a => b `IsSomeExpansionOf` a -> b -> AnyExpansion a
-lower (Inductive pf) (Lifted a) = lower pf a
-lower (Inductive _) Zero = AnyExpansionF Reflexive $ Identity 0
-lower pf a = AnyExpansionF pf $ Identity a
-
-unify :: (Eq a, Num a, Functor f, Functor g) => 
-  AnyExpansionF f a -> AnyExpansionF g a -> AnyExpansionF (Product f g) a
-AnyExpansionF Reflexive fa `unify` AnyExpansionF ay gy = AnyExpansionF ay $ Pair (lift ay <$> fa) gy
-AnyExpansionF ax fx `unify` AnyExpansionF Reflexive ga = AnyExpansionF ax $ Pair fx (lift ax <$> ga)
-AnyExpansionF ax@(Inductive _) fx `unify` AnyExpansionF ay@(Inductive _) gy = case total ax ay of
-  Left xy -> AnyExpansionF ay $ Pair (liftExpansion xy <$> fx) gy
-  Right yx -> AnyExpansionF ax $ Pair fx (liftExpansion yx <$> gy)
-
-type AnyExpansionPair = AnyExpansionF (Product Identity Identity)
-
-pattern AnyExpansion :: x `IsSomeExpansionOf` a -> x -> AnyExpansion a
-pattern AnyExpansion pf a = AnyExpansionF pf (Identity a)
-
-pattern AnyExpansionPair :: x `IsSomeExpansionOf` a -> x -> x -> AnyExpansionPair a
-pattern AnyExpansionPair pf a b = AnyExpansionF pf (Pair (Identity a) (Identity b))
-
-instance (Num a, Ord a) => Ord (AnyExpansion a) where
-  x `compare` y = 
-    case unify x y of 
-      AnyExpansionF pz (Pair (Identity xz) (Identity yz)) -> 
-        case isOrd pz of 
-          Satisfied -> compare xz yz
-
-instance (Num a, Ord a) => Eq (AnyExpansion a) where
-  x == y = case compare x y of EQ -> True ; _ -> False 
-
-instance (Ord a, Num a, Minus a) => Num (AnyExpansion a) where
-  x + y =
-    case unify x y of 
-      AnyExpansionF pz (Pair (Identity xz) (Identity yz)) -> 
-        case isNum pz of 
-          Satisfied -> AnyExpansion pz $ xz + yz
-  x * y =
-    case unify x y of 
-      AnyExpansionF pz (Pair (Identity xz) (Identity yz)) -> 
-        case isNum pz of 
-          Satisfied -> AnyExpansion pz $ xz * yz
-  x - y = case x `minus` y of
-    RightDiff _ -> error "subtraction is not closed on AnyExpansion numbers"
-    NoDiff      -> AnyExpansion Reflexive 0
-    LeftDiff γ  -> γ
-
-  abs = id
-  signum (AnyExpansionF ax (Identity x)) = case isNum ax of
-    Satisfied -> AnyExpansion Reflexive (if x == 0 then 0 else 1)
-  negate = error "negation is not defined for AnyExpansion numbers"
-  fromInteger = AnyExpansion Reflexive . fromInteger
-
-isMinus :: (Ord a, Minus a) => b `IsSomeExpansionOf` a -> Satisfied '[Ord, Minus] b
-isMinus = induction $ \Satisfied -> Satisfied
-
-instance (Num a, Ord a, Minus a) => Minus (AnyExpansion a) where
-  x `minus` y =
-    case unify x y of 
-      AnyExpansionF pz (Pair (Identity xz) (Identity yz)) -> 
-        case isMinus pz of
-          Satisfied -> 
-            case minus xz yz of
-              RightDiff c -> RightDiff $ lower pz c
-              NoDiff      -> NoDiff
-              LeftDiff c  -> LeftDiff $ lower pz c
-
-instance LPred a => LPred (AnyExpansion a) where
-  lpred (Positive (AnyExpansionF Reflexive (Identity a))) =
-    AnyExpansionF Reflexive . Identity . lpred $ Positive a
-  lpred (Positive a) = a
-
-{-
-instance LensFinite a => LensFinite (AnyExpansion a) where     
-  lensFinite f 
-  -}
-
--- instance Pow a => LensFinite (Pow a) where     
--}

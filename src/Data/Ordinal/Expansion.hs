a3aab659fa27f24017f841d41eab43f851704591
Noah Luck Easterly <noah.easterly@gmail.com>
2017-05-01 10:50:39 -0400

    Tweak external API for Expansion

--- src/Data/Ordinal/Expansion.hs
+++ src/Data/Ordinal/Expansion.hs
@@ -1,19 +1,21 @@
 {-# LANGUAGE PatternSynonyms #-}
 module Data.Ordinal.Expansion 
   ( module Data.Ordinal.Expansion.Internal
-  , pattern Expansion, pattern Lifted
+  , pattern Expansion, pattern LiftedPositive
   ) where
+
+import Data.Ordinal.Positive
+import Data.Ordinal.Zero
 
 import qualified Data.Ordinal.Expansion.Internal as Internal
 -- hide the dumb constructor and pattern from the external API
 -- as they could be used to create invariant-breaking values
-import Data.Ordinal.Expansion.Internal hiding (Expansion, Lifted)
--- but not the type
-import Data.Ordinal.Expansion.Internal (Expansion())
-import Data.Ordinal.Positive
+import Data.Ordinal.Expansion.Internal hiding (pattern Expansion, pattern Lifted)
 
-pattern Expansion :: [(Expansion a, Positive a)] -> Expansion a
-pattern Expansion ps <- Internal.Expansion ps
+-- | smart constructor
+pattern Expansion :: (Num (Expansion a)) => [(Expansion a, Positive a)] -> Expansion a
+pattern Expansion ps <- Internal.Expansion ps where
+  Expansion = sum . map (Internal.Expansion . return)
 
-pattern Lifted :: a -> Expansion a
-pattern Lifted a <- Internal.Lifted a
+pattern LiftedPositive :: (HasZero (Expansion a)) => Positive a -> Expansion a
+pattern LiftedPositive a = Internal.Expansion [(Zero, a)]
